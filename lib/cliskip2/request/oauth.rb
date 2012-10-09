1e95069a7b26bab5e007cc306912822f36418d94
maedana <maeda.na@gmail.com>
2012-10-09 19:04:15 +0900

    update to 0.11

--- lib/cliskip2/request/oauth.rb
+++ lib/cliskip2/request/oauth.rb
@@ -6,7 +6,7 @@ module Cliskip2
   module Request
     class Cliskip2OAuth < FaradayMiddleware::OAuth
       # Override Faraday::Utils#parse_nested_query for fixing base-query-string
-      # body部分のパラメタ解析方法がskip2のSPで利用しているoauth(0.4.1)とfaradayのoauthモジュールとで異なってしまい認証がうまくいかないためfaraday側のbody部分解析をoauth(0.4.1)相当に合わせている
+      # body部分のパラメタ解析方法がskip2のSPで利用しているoauth(0.4.6)とfaradayのoauthモジュールとで異なってしまい認証がうまくいかないためfaraday側のbody部分解析をoauth(0.4.6)相当に合わせている
       # See Faraday::Utils, FaradayMiddleware::OAuth
       def parse_nested_query(qs)
         params = {}
@@ -17,13 +17,6 @@ module Cliskip2
         params
       end
       def unescape(s) CGI.unescape s.to_s end
-
-      # Override Faraday::Utils#include_body_params?
-      # oauth(0.4.1)でpostの場合のみbodyをsignature-base-stringに含めているので
-      def include_body_params?(env)
-        # see RFC 5489, section 3.4.1.3.1 for details
-        env[:method] == :post && (!(type = env[:request_headers][CONTENT_TYPE]) or type == TYPE_URLENCODED)
-      end
     end
   end
 end
